b8a0c2c3d63f61504c092100fe531682950f68b8
Dudxz7 <luizeduardo98r4@gmail.com>
2024-05-19 16:13:46 -0300

    relogio digital

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const horas = document.getElementById("horas")
+const minutos = document.getElementById("minutos")
+const segundos = document.getElementById("segundos")
+
+
+const relogio = setInterval(function time(){
+
+    let data = new Date();
+    let h = data.getHours()
+    let m = data.getMinutes()
+    let s = data.getSeconds()
+
+    /*Colocando If para as horas,minutos e segundos serem de dois algarismo 01h01m01s*/
+    
+    if (h < 10) h = "0" + h
+    if (m < 10) m = "0" + m
+    if (s < 10) s = "0" + s
+
+    horas.textContent = h
+    minutos.textContent = m
+    segundos.textContent = s
+})
